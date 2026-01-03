c83a4794cbb60fc2714db492969e3afb87519214
douglasfariasrodigues <josedouglascristao23@gmail.com>
2026-01-03 15:21:20 -0300

    Primeira Atualização com melhoramento no README e as 7 primeiras questões do Beecrowd

---
+++ bee_1005.py
@@ -0,0 +1,6 @@
+valor_1 = int(input())
+valor_2 = int(input())
+
+produto = valor_1 * valor_2
+
+print(f"PROD = {produto}")
---
+++ bee_1006.py
@@ -0,0 +1,7 @@
+nota_A = float(input()) * 3.5
+nota_B = float(input()) * 7.5
+
+media = (nota_A + nota_B) / 11
+
+print(f"MEDIA = {media:0.5f}")
+
